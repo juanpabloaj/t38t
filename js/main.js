e367360f61f827fa45b6b77ebad2d503c7917efc
JuanPablo <jpabloaj@gmail.com>
2017-01-07 13:59:59 -0300

    function message with color

--- js/main.js
+++ js/main.js
@@ -5,6 +5,20 @@ var shipName;
 var z0, x0;
 var lifeSocket;
 
+// https://material.io/guidelines/style/color.html
+var COLORS = {
+  0: "#ffcdd2", 5: "#f8bbd0", 10: "#e1bee7", 15: "#d1c4e9",
+  20: "#c5cae9", 25: "#bbdefb", 30: "#b3e5fc", 35: "#b2ebf2",
+  40: "#b2dfdb", 45: "#c8e6c9", 50: "#dcedc8", 55: "#f0f4c3",
+  60: "#fff9c4", 65: "#ffecb3", 70: "#ffe0b2", 75: "#ffccbc",
+  80: "#d7ccc8", 85: "#f5f5f5", 90: "#cfd8dc", 95: "#ef9a9a",
+  100: "#f48fb1", 105: "#ce93d8", 110: "#b39ddb", 115: "#9fa8da",
+  120: "#90caf9", 125: "#81d4fa", 130: "#80deea", 135: "#80cbc4",
+  140: "#a5d6a7", 145: "#c5e1a5", 150: "#e6ee9c", 155: "#fff59d",
+  160: "#ffe082", 165: "#ffcc80", 170: "#ffab91", 175: "#bcaaa4",
+  180: "#eeeeee", 185: "#b0bec5",
+};
+
 var socketHost = "209.177.93.71";
 socketHost = "ws://" + socketHost + ":8080";
 
@@ -45,7 +59,7 @@ function init() {
       camera.position.x = x0 = lat * MTS;
       camera.rotateY((90-angle) * PI /180);
 
-      showMessage("Welcome ship" + shipName);
+      showColorMessage("Welcome ship" + shipName, COLORS[angle]);
 
       lifeSocket.send(JSON.stringify({
         message:"show collitions", name: shipName
@@ -121,12 +135,19 @@ function showCollition(data) {
 }
 
 function newShips(data) {
-  showMessage("Ship" + data.id + " detected");
+  showColorMessage("Ship" + data.id + " detected", COLORS[data.id]);
 }
 
 function showMessage(msg) {
   var msgBox = document.getElementsByClassName('log-history')[0];
   msgBox.innerHTML += new Date() + ": "+ msg + '<br />';
+  msgBox.scrollTop = msgBox.scrollHeight;
+}
+
+function showColorMessage(msg, color) {
+  var msgBox = document.getElementsByClassName('log-history')[0];
+  var div = '<div style="color:'+color+'">' + new Date() + ": "+msg+'</div>';
+  msgBox.innerHTML += div;
   msgBox.scrollTop = msgBox.scrollHeight;
 }
 
